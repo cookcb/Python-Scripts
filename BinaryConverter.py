b469d8c1a5761f39b197e6e866967c4f9f5e1357
Christian <cbcook321@gmail.com>
2016-05-17 21:27:11 -0400

    Added some input code

---
+++ BinaryConverter.py
@@ -0,0 +1,5 @@
+#READ ME
+#This is a Python Script that will convert a given number to Binary and vice versa
+
+value = input('Enter the binary value that you would like to convert to a whole number: ');
+print('Binary Value: ', value);
