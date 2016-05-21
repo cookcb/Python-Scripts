bd90e870f22e27fcb12d6ce128148b197c251aa0
Christian <cbcook321@gmail.com>
2016-05-21 01:13:10 -0400

    Added code to determine length of input value

--- BinaryConverter.py
+++ BinaryConverter.py
@@ -2,3 +2,5 @@
 #This is a Python Script that will convert a given number to Binary and vice versa
 
 value = input('Enter the binary value that you would like to convert to a whole number: ')
+length = len(str(value))
+print length
