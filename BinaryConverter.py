382c0f7c5bdbcd03190be739609efc88bd5df32f
Christian <cbcook321@gmail.com>
2016-05-21 00:56:48 -0400

    Deleted Print Statement

--- BinaryConverter.py
+++ BinaryConverter.py
@@ -1,5 +1,4 @@
 #READ ME
 #This is a Python Script that will convert a given number to Binary and vice versa
 
-value = input('Enter the binary value that you would like to convert to a whole number: ');
-print('Binary Value: ', value);
+value = input('Enter the binary value that you would like to convert to a whole number: ')
