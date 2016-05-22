1d55660ff65a2b42c30db4b3d44f2d23443a9952
Christian <cbcook321@gmail.com>
2016-05-22 16:47:06 -0400

    Fixed input error

--- BinaryConverter.py
+++ BinaryConverter.py
@@ -2,21 +2,19 @@
 #This is a Python Script that will convert a given number to Binary and vice versa
 #Still currently being worked on
 
-response = ('Type "one" for binary -> whole number and "two" for whole number -> binary: ')
+response = raw_input('Type "one" for binary -> whole number and "two" for whole number -> binary: ')
 
 value = input('Enter the value that you would like to convert: ')
 
 def convert(value, response):
 	count = 0
 	total = 0
-	if response == 'one'
+	if response == 'one':
 		for digit in str(value):
 			if digit == '1':
 				total = total + (2**count)
 			count += 1	
-	else:
-		
-	return total
+	return total;
 
 
 print convert(value, response)
