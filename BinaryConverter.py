d89e96d1170954667c0eaae2b10b650ad55559c9
Christian Cook <cbcook321@gmail.com>
2016-08-01 23:34:11 -0400

    removed unused lines

--- BinaryConverter.py
+++ BinaryConverter.py
@@ -33,9 +33,9 @@ B1 = Radiobutton(top, text = "Decimal to Binary", variable = v, value = 1, indic
 
 B2 = Radiobutton(top, text = "Binary to Decimal", variable = v, value = 2, indicatoron = 0).grid(row = 0, column = 1)
 
-EntryField = Entry(top).grid(row = 1, column = 1)
+#EntryField = Entry(top).grid(row = 1, column = 1)
 
-Convert = Button(top, text = "Convert", command = lambda:convert(EntryField.get(), v.get())).grid(row = 2, columnspan = 2)
+Convert = Button(top, text = "Convert", command = lambda:convert(10, v.get())).grid(row = 2, columnspan = 2)
 label = Label(top, text = "Decimal/Binary Value").grid(row = 1, column = 0)
 
 
