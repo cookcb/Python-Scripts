e1e34afcee4dd9b215d0450fb3ff3d6f22394e62
Christian Cook <cbcook321@gmail.com>
2016-08-07 00:59:47 -0400

    Restructured code to be surrounded by class

--- BinaryConverter.py
+++ BinaryConverter.py
@@ -3,45 +3,47 @@
 #Still currently being worked on
 from Tkinter import *
 
+class BinaryConverter():
+	def convert(value, response):
+		count = 0
+		total = 0
+		if value == "":
+			total = "No Entry"
+		elif response == 1:
+			for digit in str(value):
+				if digit == '1':
+					total = total + (2**count)
+				count += 1	
+		else:
+			total = ''
+			#While loop to get the highest base that the value can go into
+			while (value > 2**count):
+				count +=1
+			#for loop that counts backwards from the count value
+			for place in range(count, 0, -1):
+				if value >= 2**(place-1):
+					total = total + '1'
+					#To calculate the newly created value
+					value = value - 2**(place-1)
+				else:
+					total = total + '0'
+		print total;
 
-def convert(value, response):
-	count = 0
-	total = 0
-	if value == "":
-		total = "No Entry"
-	elif response == 1:
-		for digit in str(value):
-			if digit == '1':
-				total = total + (2**count)
-			count += 1	
-	else:
-		total = ''
-		#While loop to get the highest base that the value can go into
-		while (value > 2**count):
-			count +=1
-		#for loop that counts backwards from the count value
-		for place in range(count, 0, -1):
-			if value >= 2**(place-1):
-				total = total + '1'
-				#To calculate the newly created value
-				value = value - 2**(place-1)
-			else:
-				total = total + '0'
-	print total;
+	top = Tk()
+	v = IntVar()
+	B1 = Radiobutton(top, text = "Decimal to Binary", variable = v, value = 1, indicatoron = 0).grid(row = 0, column = 0)
 
-top = Tk()
-v = IntVar()
-B1 = Radiobutton(top, text = "Decimal to Binary", variable = v, value = 1, indicatoron = 0).grid(row = 0, column = 0)
+	B2 = Radiobutton(top, text = "Binary to Decimal", variable = v, value = 2, indicatoron = 0).grid(row = 0, column = 1)
 
-B2 = Radiobutton(top, text = "Binary to Decimal", variable = v, value = 2, indicatoron = 0).grid(row = 0, column = 1)
-
-EntryField = Entry(top)
-EntryField.grid(row = 1, column = 1)
+	EntryField = Entry(top)
+	EntryField.grid(row = 1, column = 1)
 
 
-Convert = Button(top, text = "Convert", command = lambda:convert(EntryField.get(), v.get())).grid(row = 2, columnspan = 2)
-label = Label(top, text = "Decimal/Binary Value").grid(row = 1, column = 0)
-Result = Label(top, text = "Result:").grid(row = 3, column = 0)
+	Convert = Button(top, text = "Convert", command = lambda:convert(EntryField.get(), v.get())).grid(row = 2, columnspan = 2)
+	label = Label(top, text = "Decimal/Binary Value").grid(row = 1, column = 0)
+	Result = Label(top, text = "Result:").grid(row = 3, column = 0)
 
-top.mainloop()
+	top.mainloop()
+
+
 
