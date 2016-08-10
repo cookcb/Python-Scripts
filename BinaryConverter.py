faccbff94f9c79293fcea5ede9670d5c005e69d4
Christian Cook <cbcook321@gmail.com>
2016-08-09 21:25:39 -0400

    Calling tk from import

--- BinaryConverter.py
+++ BinaryConverter.py
@@ -1,9 +1,9 @@
 #READ ME
 #This is a Python Script that will convert a given  decimal number to Binary and vice versa
 #Still currently being worked on
-from Tkinter import *
+import Tkinter as tk
 
-class BinaryConverter(root):
+class BinaryConverter(tk.Frame):
 	def convert(value, response):
 		count = 0
 		total = 0
@@ -29,22 +29,22 @@ class BinaryConverter(root):
 					total = total + '0'
 		print total;
 
-#	top = Tk()
-	v = IntVar()
-	B1 = Radiobutton(top, text = "Decimal to Binary", variable = v, value = 1, indicatoron = 0).grid(row = 0, column = 0)
+	top = tk.Tk()
+	v = tk.IntVar()
+	B1 = tk.Radiobutton(tk.Frame, text = "Decimal to Binary", variable = v, value = 1, indicatoron = 0).grid(row = 0, column = 0)
 
-	B2 = Radiobutton(top, text = "Binary to Decimal", variable = v, value = 2, indicatoron = 0).grid(row = 0, column = 1)
+	B2 = tk.Radiobutton(tk.Frame, text = "Binary to Decimal", variable = v, value = 2, indicatoron = 0).grid(row = 0, column = 1)
 
-	EntryField = Entry(top)
+	EntryField = tk.Entry(tk.Frame)
 	EntryField.grid(row = 1, column = 1)
 
 
-	Convert = Button(top, text = "Convert", command = lambda:convert(EntryField.get(), v.get())).grid(row = 2, columnspan = 2)
-	label = Label(top, text = "Decimal/Binary Value").grid(row = 1, column = 0)
-	Result = Label(top, text = "Result:").grid(row = 3, column = 0)
+	Convert = Button(tk.Frame, text = "Convert", command = lambda:convert(EntryField.get(), v.get())).grid(row = 2, columnspan = 2)
+	label = Label(tk.Frame, text = "Decimal/Binary Value").grid(row = 1, column = 0)
+	Result = Label(tk.Frame, text = "Result:").grid(row = 3, column = 0)
 
 top = Tk()
-
+BinaryConverter(top);
 top.mainloop()
 
 
